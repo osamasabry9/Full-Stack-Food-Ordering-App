0d858f92b4c10d95ac8bc07930f2c679b372cf52
osamasabry9 <osamasabry324@gmail.com>
2025-01-24 15:34:35 +0400

    Add slider icon and adjust Hero component layout
    Add a new slider icon SVG file to the assets/icons directory. Update the Hero component to include the slider icon below the text content and adjust the layout for better alignment. The hero image container is also repositioned slightly to the right for improved spacing.

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -8,48 +8,55 @@ import React from "react";
 const Hero = () => {
   return (
     <section className="section-gap grid grid-cols-1 md:grid-cols-2">
-        {/* Text  */}
-        <div className="md:py-16 ">
-          <h1 className="text-4xl font-semibold">
-            We&apos;re <span className="text-primary">Serious</span> For <br />
-            <span className="text-primary">Food</span> &{" "}
-            <span className="text-[#FDC55E]">Delivery.</span>
-          </h1>
-          <p className="text-accent my-8">
-            Best cooks and best delivery guys all at your service. Hot tasty
-            food will reach you in 60 minutes.
-          </p>
-          <div className="flex items-center gap-4 my-8">
-            <Link
-              href={`/${Routes.MENU}`}
-              className={`${buttonVariants({
-                size: "lg",
-              })} space-x-2 !px-4 !rounded-full uppercase`}
-            >
-              Order Now
-              <ArrowRightCircle className={`!w-5 !h-5 `} />
-            </Link>
-            <Link
-              href={`/${Routes.ABOUT}`}
-              className="flex gap-2 items-center text-black hover:text-primary duration-200 transition-colors font-semibold"
-            >
-              Learn More
-              <ArrowRightCircle className={`!w-5 !h-5 `} />
-            </Link>
-          </div>
+      {/* Text  */}
+      <div className="md:py-16 ">
+        <h1 className="text-4xl font-semibold">
+          We&apos;re <span className="text-primary">Serious</span> For <br />
+          <span className="text-primary">Food</span> &{" "}
+          <span className="text-[#FDC55E]">Delivery.</span>
+        </h1>
+        <p className="text-accent my-8">
+          Best cooks and best delivery guys all at your service. Hot tasty food
+          will reach you in 60 minutes.
+        </p>
+        <div className="flex items-center gap-4 my-8">
+          <Link
+            href={`/${Routes.MENU}`}
+            className={`${buttonVariants({
+              size: "lg",
+            })} space-x-2 !px-4 !rounded-full uppercase`}
+          >
+            Order Now
+            <ArrowRightCircle className={`!w-5 !h-5 `} />
+          </Link>
+          <Link
+            href={`/${Routes.ABOUT}`}
+            className="flex gap-2 items-center text-black hover:text-primary duration-200 transition-colors font-semibold"
+          >
+            Learn More
+            <ArrowRightCircle className={`!w-5 !h-5 `} />
+          </Link>
         </div>
-        {/* Hero image */}
-        <div className="relative hidden md:block ">
-          <Image
-            src="/assets/images/hero_image.svg"
-            alt="hero image"
-            fill
-            className="object-contain"
-            loading="eager"
-            priority
-          />
-        </div>
-
+        <Image
+          src="/assets/icons/slider.svg"
+          alt="slider icon"
+          width={22}
+          height={61}
+          className="object-contain md:pt-20"
+          loading="lazy"
+        />
+      </div>
+      {/* Hero image */}
+      <div className="relative hidden md:block md:right-[-50px] ">
+        <Image
+          src="/assets/images/hero_image.svg"
+          alt="hero image"
+          fill
+          className="object-contain"
+          loading="eager"
+          priority
+        />
+      </div>
     </section>
   );
 };
