3ac6ebcb2c0f7ea7bbc9eb36e79286afa1b5d6cf
osamasabry9 <osamasabry324@gmail.com>
2025-02-03 00:34:59 +0400

    Add About page. Add About component and display on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./_components/Hero";
 import BestSellersSection from "./_components/BestSellers";
+import About from "@/components/about/About";
 
 export default function Home() {
   return (
     <main className="container">
       <Hero/>
       <BestSellersSection/>
+      <About/>
     </main>
   );
 }
