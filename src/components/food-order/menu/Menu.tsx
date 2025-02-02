a79528f4dc46b8a3680f4e2c59a37015736abbee
osamasabry9 <osamasabry324@gmail.com>
2025-02-03 00:21:57 +0400

    Get Bes tSellers from Database. replace static data to dynamic data from Database update data type of product

--- src/components/food-order/menu/Menu.tsx
+++ src/components/food-order/menu/Menu.tsx
@@ -1,47 +1,25 @@
-import { TMenuItem } from "@/types";
 import React from "react";
 import MenuItem from "./MenuItem";
+import { TProductWithRelations } from "@/types";
 
-const MenuItems: TMenuItem[] = [
-  {
-    id: 1,
-    title: "Pizza",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    price: 100,
-    image: "/assets/images/pizza.svg",
+type TProps = {
+  items: TProductWithRelations[];
+};
 
+ function Menu({ items }: TProps) {
 
-  },
-  {
-    id: 2,
-    title: "Pizza",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    price: 100,
-    image: "/assets/images/pizza.svg",
-
-  },
-  {
-    id: 3,
-    title: "Pizza",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    price: 100,
-    image: "/assets/images/pizza.svg",
-  },
-];
-
-const Menu = () => {
-  return (
+  return items.length > 0 ? (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-      {MenuItems.map((item) => (
+      {items.map((item) => (
         <div
           key={item.id}
           className="flex justify-center transform transition-transform duration-300 hover:scale-105 pb-8"
         >
-          <MenuItem {...item} />
+          <MenuItem product={item} />
         </div>
       ))}
     </div>
-  );
+  ) : <p className="text-center text-muted-foreground" >No products available.</p>;
 };
 
 export default Menu;
--- src/components/food-order/menu/MenuItem.tsx
+++ src/components/food-order/menu/MenuItem.tsx
@@ -1,10 +1,12 @@
 import { formatCurrency } from "@/lib/formatters";
-import { TMenuItem } from "@/types";
 import Image from "next/image";
 import React from "react";
 import OrderNowButton from "./OrderNowButton.2";
 
-const MenuItem = ({ id, title, description, price, image }: TMenuItem) => {
+import { TProductWithRelations } from "@/types";
+const MenuItem = ({ product }: { product: TProductWithRelations }) => {
+  const {name, description, basePrice, image } = product;
+
   return (
     <article className="relative flex h-[400px] w-[80%] max-w-xs flex-col items-center rounded-2xl bg-gradient-to-b from-white from-40% to-primary/10 shadow-lg transition-transform duration-300 hover:scale-[1.02] hover:shadow-xl  md:max-w-sm">
       {/* Image Section with Gradient Border */}
@@ -13,7 +15,7 @@ const MenuItem = ({ id, title, description, price, image }: TMenuItem) => {
           <div className="relative h-full w-full overflow-hidden rounded-full border-2 border-white md:border-[3px]">
             <Image
               src={image}
-              alt={`${title} dish`}
+              alt={`${name} dish`}
               fill
               className="object-cover"
               sizes="(max-width: 640px) 50vw, (max-width: 1024px) 33vw, 20vw"
@@ -27,14 +29,14 @@ const MenuItem = ({ id, title, description, price, image }: TMenuItem) => {
           className="absolute right-[5%] bottom-[10%] z-10 flex aspect-square w-[30%] min-w-[60px] items-center justify-center rounded-full border-2 border-white bg-[#FDC55E] text-[clamp(0.60rem,2vw,0.90rem)] font-bold text-white
         shadow-md sm:right-[7%] sm:bottom-[10%] md:text-[clamp(0.65rem,1.5vw,0.95rem)]"
         >
-          <span>{formatCurrency(price)}</span>
+          <span>{formatCurrency(basePrice)}</span>
         </div>
       </div>
 
       {/* Content Section */}
       <div className="flex flex-1 flex-col text-center px-4 py-2 sm:px-6 sm:py-4">
         <h2 className="text-lg font-bold text-primary sm:text-xl md:text-2xl">
-          {title}
+          {name}
         </h2>
         <p className="line-clamp-3 mt-1 text-xs text-gray-600 sm:mt-2 sm:text-sm md:text-base">
           {description}
@@ -42,7 +44,7 @@ const MenuItem = ({ id, title, description, price, image }: TMenuItem) => {
       </div>
 
       {/* Floating Action Button */}
-      <OrderNowButton item={{ id, title, description, price, image }} />
+      <OrderNowButton item={product} />
     </article>
   );
 };
