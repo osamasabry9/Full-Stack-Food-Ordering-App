7215410a9d578f001436991648ece077bbc1ae87
osamasabry9 <osamasabry324@gmail.com>
2025-02-08 23:45:50 +0400

    Add delivery fee and debounce utility
    Introduce a `deliveryFee` constant and a `getTotalAmount` function to calculate the total price including delivery in the cart module. Additionally, create a new `debounce` utility function to delay the execution of a function, useful for optimizing performance in scenarios like handling user input.

--- src/lib/cart.ts
+++ src/lib/cart.ts
@@ -8,9 +8,17 @@ export const getItemQuantity = (cartItems: TCartItem[], id: string) => {
   return cartItems.find((item) => item.id === id)?.quantity || 0;
 };
 
+export const deliveryFee = 5;
+
 export const getTotalPrice = (cartItems: TCartItem[]) => {
   return cartItems.reduce(
     (total, item) => total + (item.quantity || 0) * item.basePrice,
     0
   );
 };
+
+export const getTotalAmount = (cartItems: TCartItem[]) => {
+  return getTotalPrice(cartItems) + deliveryFee ;
+};
+
+
